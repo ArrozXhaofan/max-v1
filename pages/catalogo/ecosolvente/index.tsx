a433d72399535c19ec9ab63aea089a42fd7d444f
ArrozXhaofan <jean.ganador12@gmail.com>
2023-05-05 18:50:18 -0500

    ds

--- pages/catalogo/ecosolvente/index.tsx
+++ pages/catalogo/ecosolvente/index.tsx
@@ -1,11 +1,10 @@
 import React from "react";
-import Layout from "../Layout";
 
 function Ecosolvente() {
   return (
-    <Layout>
+    
       <div>Ecosolvente</div>
-    </Layout>
+    
   );
 }
 
