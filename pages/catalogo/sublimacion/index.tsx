78407e9f3f8e40b30695f22330197e3214579d30
ArrozXhaofan <jean.ganador12@gmail.com>
2023-05-05 18:52:20 -0500

    aaaa

--- pages/catalogo/sublimacion/index.tsx
+++ pages/catalogo/sublimacion/index.tsx
@@ -1,14 +1,13 @@
 import React from 'react'
-import Layout from '../Layout'
+
 
 
 function Sublimacion() {
   return (
     
-    <Layout>
+ 
         <div>Sublimacion</div>
-    </Layout>
-
+  
     
    
     
